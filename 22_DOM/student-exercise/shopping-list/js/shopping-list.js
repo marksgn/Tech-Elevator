c399abce1754d145a91843dccdfa967dad4871e2
marksgn <68515019+marksgn@users.noreply.github.com>
2020-08-26 13:37:55 -0400

    Add files via upload

---
+++ 22_DOM/student-exercise/shopping-list/js/shopping-list.js
@@ -0,0 +1,57 @@
+// add pageTitle
+let pageTitle = 'My Shopping List';
+
+// add groceries
+const groceries = [
+  { name : 'Chips'},
+  { name : 'Soda'},
+  { name : 'Chocolate'},
+  { name : 'Rice'},
+  { name : 'Pretzels'},
+  { name : 'Beer'},
+  { name : 'Milk'},
+  { name : 'Eggs'},
+  { name : 'Cheese'},
+  { name : 'Bread'}
+]
+
+/**
+ * This function will get a reference to the title and set its text to the value
+ * of the pageTitle variable that was set above.
+ */
+function setPageTitle() {
+    const title = document.getElementById('title');
+    title.innerText = pageTitle;
+}
+
+/**
+ * This function will loop over the array of groceries that was set above and add them to the DOM.
+ */
+function displayGroceries() {
+  const ul = document.getElementById('groceries');
+  groceries.forEach((item) => {
+    const li = document.createElement('li');
+    li.innerText = item.name;
+    ul.appendChild(li);
+  });
+}
+
+/**
+ * This function will be called when the button is clicked. You will need to get a reference
+ * to every list item and add the class completed to each one
+ */
+function markCompleted() {
+  const allTheGroceries = document.querySelectorAll('li')
+  allTheGroceries.forEach( (item) => item.classList.add('completed'))
+}
+
+setPageTitle();
+
+displayGroceries();
+
+// Don't worry too much about what is going on here, we will cover this when we discuss events.
+document.addEventListener('DOMContentLoaded', () => {
+  // When the DOM Content has loaded attach a click listener to the button
+  const button = document.querySelector('.btn');
+  button.addEventListener('click', markCompleted);
+});
